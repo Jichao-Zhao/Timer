e084cbd9cbf54be6b92353833cc3decc6408a2f9
478984342@qq.com <478984342@qq.com>
2020-09-09 12:46:57 +0800

    Add files

---
+++ jishiqi/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace jishiqi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始计时";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(102, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 22);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 14);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "时长";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 14);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "剩余时间：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0秒";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 114);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(492, 25);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 160);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "计时器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ jishiqi/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace jishiqi
+{
+    public partial class Form1 : Form
+    {
+        int count;                                                  //定义一个全局变量
+        int time;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i;
+            for (i = 1; i < 100; i++)                               //计数范围（0~99）
+            {
+                comboBox1.Items.Add(i.ToString() + " 秒");          //初始化下拉框内容（数字后加一个空格便于程序运行）
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)      //点击按钮，开始计时按钮事件
+        {
+            string str = comboBox1.Text;                            //将下拉框内容添加到一个变量中
+            string data = str.Substring(0, 2);                      //得到设定定时值（整形）
+            time = Convert.ToInt16(data);
+            progressBar1.Maximum = time;                            //进度条最大值
+            timer1.Start();                                         //开始计时
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)        //定时器事件
+        {
+            count++;                                                //记录当前秒
+            label3.Text = (time - count).ToString() + "秒";         //显示剩余时间
+            progressBar1.Value = count;                             //设置进度条进度
+            if (count == time)
+            {
+                timer1.Stop();                                      //时间到，停止计时
+                System.Media.SystemSounds.Asterisk.Play();          //提示音
+                MessageBox.Show("时间到了！", "提示");              //弹出提示框
+            }
+        }
+    }
+}
